0112f268b85f0b5741d5fa7371d59dd0af42ec65
Liz <enanthav@gmail.com>
2017-11-08 14:12:59 -0700

    Add ChangeVideo.cs script

---
+++ Nurse Simulator/Assets/360 Video Player/Scripts/ChangeVideo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+
+public class ChangeVideo : MonoBehaviour {
+
+	public VideoPlayer videoPlayer;
+	public VideoClip[] videos = new VideoClip[5];
+
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void PlayRounding () {
+		SwitchVideo (1);
+	}
+
+	// AGGRESSIVE VIDEO
+	public void PlayAggressive () { 
+		SwitchVideo (2);
+	}
+	// OPTIONS FOR AGGRESSIVE VIDEO
+	public void PlayAggressiveDistraction () {
+		SwitchVideo (3);
+	}
+
+	public void PlayEndOfShift () {
+		SwitchVideo (4);
+	}
+
+
+	public void SwitchVideo(int index)
+	{
+		videoPlayer.clip = videos[index];
+		videoPlayer.Play();
+	}
+}
